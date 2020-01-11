130af07a8d10667b1d4383b8e6f5dda89bca1967
Jonas Timmermann <36668988+JonasTimmermann@users.noreply.github.com>
2020-01-12 00:00:16 +0100

    CrossOrigindelete

--- src/main/java/com/example/gewerbeanmeldung/Question/QuestionController.java
+++ src/main/java/com/example/gewerbeanmeldung/Question/QuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin  //(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("")
 public class QuestionController {
