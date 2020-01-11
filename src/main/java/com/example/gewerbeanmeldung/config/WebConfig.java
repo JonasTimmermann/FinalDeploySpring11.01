2f811804dece11ffa8e2fb6270beff4c26330d67
Jonas Timmermann <stu210066@mail.uni-kiel.de>
2020-01-11 23:30:49 +0100

    Deployed11.01WithStartANDKategorieANDLogin

---
+++ src/main/java/com/example/gewerbeanmeldung/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.gewerbeanmeldung.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+
+@Override
+public void addCorsMappings(CorsRegistry corsRegistry){
+	corsRegistry.addMapping("/**")
+	.allowedOrigins("*")
+    .allowedMethods("*")
+    .maxAge(3600L)
+	.allowedHeaders("*")
+	.exposedHeaders("Authorization")
+	.allowCredentials(true);
+}
+
+}
