113cffe701a9f9c4155c18d6048055e8ec18d1ff
Jonas Timmermann <36668988+JonasTimmermann@users.noreply.github.com>
2020-01-12 02:11:43 +0100

    Bean

--- src/main/java/com/example/gewerbeanmeldung/config/WebConfig.java
+++ src/main/java/com/example/gewerbeanmeldung/config/WebConfig.java
@@ -1,8 +1,14 @@
 package com.example.gewerbeanmeldung.config;
 
+import java.util.Arrays;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,6 +27,25 @@ public void addCorsMappings(CorsRegistry corsRegistry){
 	.allowedHeaders("*")
 	.exposedHeaders("Authorization")
 	.allowCredentials(true);
+}
+	
+@Bean
+public CorsFilter corsFilter() {
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true); 
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("OPTIONS");
+    config.addAllowedMethod("HEAD");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("DELETE");
+    config.addAllowedMethod("PATCH");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
 }
 
 }
